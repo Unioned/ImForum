0f58e712327074d77415efe0675735bc662aa059
lizhenzhou <2314383370@qq.com>
2023-03-13 15:16:33 +0800

    Initial commit

---
+++ src/main/java/com/example/imforum/ImForumApplication.java
@@ -0,0 +1,13 @@
+package com.example.imforum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImForumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImForumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/imforum/ImForumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.imforum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImForumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
